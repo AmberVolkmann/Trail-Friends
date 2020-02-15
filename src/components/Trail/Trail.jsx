cc63c7c53148e13d7e4acc037b5e90d162e1a490
Amber Volkmann <volkmannamber@gmail.com>
2020-02-15 12:48:23 -0600

    edited card component to display correct information from API

--- src/components/Trail/Trail.jsx
+++ src/components/Trail/Trail.jsx
@@ -49,20 +49,14 @@ class Trail extends Component {
 
   state = { expanded: false };
 
-    // componentDidMount() {
-    //     this.props.dispatch({
-    //         type: 'FETCH_TRAILS'
-    //     })
-        
-    // }
+    handleExpandClick = () => {
+        this.setState(state => ({ expanded: !state.expanded }));
+      };
 
 
   render() {
     const { classes } = this.props;
 
-    // let id = this.props.id
-    // let index = id -1
-    // let trail = this.props.reduxStore.trailReducer[index]
 
     return (
     <Router>
@@ -71,7 +65,7 @@ class Trail extends Component {
                     <CardHeader
                       avatar={
                         <Avatar aria-label="Recipe" className={classes.avatar}>
-                          R
+                          TF
                         </Avatar>
                       }
                       action={
@@ -96,9 +90,9 @@ class Trail extends Component {
                       <IconButton aria-label="Add to favorites">
                         <FavoriteIcon />
                       </IconButton>
-                      <IconButton aria-label="Share">
+                      {/* <IconButton aria-label="Share">
                         <ShareIcon />
-                      </IconButton>
+                      </IconButton> */}
                       <IconButton
                         className={classnames(classes.expand, {
                           [classes.expandOpen]: this.state.expanded,
@@ -112,28 +106,22 @@ class Trail extends Component {
                     </CardActions>
                     <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
                       <CardContent>
-                        <Typography paragraph>Method:</Typography>
+                        <Typography variant="h6">Details</Typography>
+                        <Typography variant="subtitle1">Length:</Typography>
                         <Typography paragraph>
-                          Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                          minutes.
+                          {this.props.trail.length} miles
                         </Typography>
+                        <Typography variant="subtitle1">Ascent:</Typography>
                         <Typography paragraph>
-                          Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                          heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                          browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                          chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                          salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                          minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                          {this.props.trail.ascent}
                         </Typography>
+                        <Typography variant="subtitle1">Descent:</Typography>
                         <Typography paragraph>
-                          Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                          without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-                          to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-                          cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                          minutes more. (Discard any mussels that don’t open.)
+                          {this.props.trail.descent} 
                         </Typography>
-                        <Typography>
-                          Set aside off of the heat to let rest for 10 minutes, and then serve.
+                        <Typography variant="subtitle1">Condition:</Typography>
+                        <Typography paragraph>
+                          {this.props.trail.conditionDetails}
                         </Typography>
                       </CardContent>
                     </Collapse>
