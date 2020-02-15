b18752d1001858f4e3cd559b3031ef23cb96a475
Amber Volkmann <volkmannamber@gmail.com>
2020-02-15 12:30:23 -0600

    trail moved to it's own component. mapping over in all trails component now

--- src/components/TrailDetail/TrailDetail.jsx
+++ src/components/TrailDetail/TrailDetail.jsx
@@ -8,8 +8,28 @@ import { Link } from 'react-router-dom';
 class TrailDetail extends Component {
     render () {
         return (
-            <>
-            </>
+            <div
+        className="trail-item"
+        onClick={evt => {
+          this.props.onClick(evt, this.props.trail);
+        }}
+      >
+        <div className="trail-img">
+          <img src={this.props.checkTrailImage(this.props.trail)} alt={this.props.trail.name} title={this.props.trail.name}/>
+        </div>
+        <div className="trail-details">
+          <h3 title={this.props.trail.name} tabIndex="0">
+            {this.props.trail.name}
+          </h3>
+          <p>
+            Length: {this.props.trail.length} miles <br />
+            Difficulty: {this.props.checkDifficulty(this.props.trail)}
+          </p>
+          <p>{this.props.trail.summary}</p>
+
+          
+        </div>
+        </div>
         )
     }
 }
